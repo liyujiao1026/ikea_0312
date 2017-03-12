599655f92f6a88595c8a22adc11f7570f740de07
Yujiao Li <liyujiao1026@163.com>
2017-03-12 13:24:42 +0100

    Update 0_source.R

--- 0_source.R
+++ 0_source.R
@@ -4,10 +4,10 @@ library(compiler)
 library(dplyr)
 library(kohonen)
 
-
-source('./1_Func_SCM.R', echo = F)
-source('./2_Func_cluster.R', echo = F)
-source('./3_Func_bootstrapSCM.R', echo = F)
-source('./4_Func_parametric.R', echo = F)
-source('./5_Func_paper.R', echo = F)
+path <- "https://raw.githubusercontent.com/liyujiao1026/ikea_0312/master/"
+source(paste0(path,'1_Func_SCM.R'), echo = F)
+source(paste0(path,'./2_Func_cluster.R'), echo = F)
+source(paste0(path,'./3_Func_bootstrapSCM.R'), echo = F)
+source(paste0(path,'./4_Func_parametric.R'), echo = F)
+source(paste0(path,'./5_Func_paper.R'), echo = F)
 
